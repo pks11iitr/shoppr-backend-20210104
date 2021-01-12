e8957eefc6156b68014a0be45d9836814a6dba55
pankaj <pankaj@vm-jenkins-new.com>
2021-01-12 18:27:22 +0530

    chat apis - text,image,audio

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Traits\DocumentUploadTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class ChatMessage extends Model
 {
@@ -13,5 +14,11 @@ class ChatMessage extends Model
     protected $table='chatmessages';
 
     protected $fillable=['chat_id', 'type', 'message', 'file_path', 'direction', 'status', 'price', 'quantity'];
+
+    public function getDocPathAttribute($value){
+        if($value)
+            return Storage::url($value);
+        return '';
+    }
 
 }
