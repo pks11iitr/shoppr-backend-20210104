363ba112f4a751b69730f1f5f644d9b5b5500fb7
pankaj <pankaj@vm-jenkins-new.com>
2021-01-20 13:37:17 +0530

    fcm notifications setup

--- app/Http/Controllers/MobileApps/Api/ChatMessageController.php
+++ app/Http/Controllers/MobileApps/Api/ChatMessageController.php
@@ -75,7 +75,7 @@ class ChatMessageController extends Controller
             case 'product':
                 $message=ChatMessage::create([
                     'chat_id'=>$chat_id,
-                    'message'=>$request->address,
+                    'message'=>'',
                     'type'=>'product',
                     'price'=>$request->price,
                     'quantity'=>$request->quantity,
