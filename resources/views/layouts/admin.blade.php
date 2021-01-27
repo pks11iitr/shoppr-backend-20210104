dd78057fd0d49e6c3c25fc1dbaf41d4387ca3434
alam <badre.alam1993@gmail.com>
2021-01-27 19:24:48 +0530

    wallet history api fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Arogya</title>
+    <title>Shoppr</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
@@ -33,7 +33,7 @@
     <!-- Navbar -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
         <!-- Left navbar links -->
-        <ul class="navbar-nav">
+       {{-- <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
@@ -44,9 +44,9 @@
                 <a href="#" class="nav-link">Contact</a>
             </li>
         </ul>
-
+--}}
         <!-- SEARCH FORM -->
-        <form class="form-inline ml-3">
+      {{--  <form class="form-inline ml-3">
             <div class="input-group input-group-sm">
                 <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
                 <div class="input-group-append">
@@ -55,7 +55,7 @@
                     </button>
                 </div>
             </div>
-        </form>
+        </form>--}}
 
         <!-- Right navbar links -->
         <ul class="navbar-nav ml-auto">
@@ -73,11 +73,11 @@
                     </form>
                 </div>
             </li>
-            <li class="nav-item">
+           {{-- <li class="nav-item">
                 <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
                     <i class="fas fa-th-large"></i>
                 </a>
-            </li>
+            </li>--}}
         </ul>
     </nav>
     <!-- /.navbar -->
@@ -85,23 +85,23 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="{{route('home')}}" class="brand-link">
            <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">-->
-            <span class="brand-text font-weight-light">Shaadi Bandhan CMS</span>
+            <span class="brand-text font-weight-light">Shoppr CMS</span>
         </a>
 
         <!-- Sidebar -->
         <div class="sidebar">
             <!-- Sidebar user panel (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
+          {{--  <div class="user-panel mt-3 pb-3 mb-3 d-flex">--}}
+                {{--<div class="image">
                     <img src="{{\Illuminate\Support\Facades\Storage::url('images/logo.jpeg')}}" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
+                </div>--}}
+               {{-- <div class="info">
                     <a href="#" class="d-block">Shaadi Bandhan</a>
-                </div>
-            </div>
+                </div>--}}
+          {{--  </div>--}}
 
             <!-- Sidebar Menu -->
             <nav class="mt-2">
