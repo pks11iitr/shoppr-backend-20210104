e2eba9d912d87b7da79831d7f18de3885b0f2611
pankaj <pankaj@vm-jenkins-new.com>
2021-02-02 13:14:58 +0530

    integrating video calls

--- app/Http/Controllers/MobileApps/Api/CallController.php
+++ app/Http/Controllers/MobileApps/Api/CallController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\MobileApps\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chat;
 use App\Models\Shoppr;
 use App\Services\Agora\RtcTokenBuilder;
 use App\Services\Notification\FCMNotification;
@@ -15,15 +16,18 @@ class CallController extends Controller
 
     }
 
-    public function initiateVideocall(Request $request, $user_id){
+    public function initiateVideocall(Request $request, $chat_id){
 
         $user=$request->user;
-        $receiver=Shoppr::findOrFail($user_id);
+
+        $chat=Chat::with('shoppr')
+            ->findOrFail($chat_id);
+        //$receiver=Shoppr::findOrFail($user_id);
 
         $channel_name='customerchannel'.$user->id;
         $token=RtcTokenBuilder::buildTokenWithUid($channel_name, $user->id,1,0);
 
-        $receiver->notify(new FCMNotification('Calling..', 'Call from '.$user->name, [
+        $chat->shoppr->notify(new FCMNotification('Calling..', 'Call from '.$user->name, [
             'token'=>$token,
             'channel'=>$channel_name,
             'caller'=>$user->name,
--- app/Http/Controllers/MobileApps/ShopprApp/CallController.php
+++ app/Http/Controllers/MobileApps/ShopprApp/CallController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\MobileApps\ShopprApp;
 
 use App\Http\Controllers\Controller;
+use App\Models\Chat;
 use App\Models\Customer;
 use App\Models\Shoppr;
 use App\Services\Agora\RtcTokenBuilder;
@@ -11,15 +12,17 @@ use Illuminate\Http\Request;
 
 class CallController extends Controller
 {
-    public function initiateVideocall(Request $request, $user_id){
+    public function initiateVideocall(Request $request, $chat_id){
 
         $user=$request->user;
-        $receiver=Customer::findOrFail($user_id);
+        //$receiver=Customer::findOrFail($user_id);
+        $chat=Chat::with('customer')
+            ->findOrFail($chat_id);
 
         $channel_name='shopprchannel'.$user->id;
         $token=RtcTokenBuilder::buildTokenWithUid($channel_name, $user->id,1,0);
 
-        $receiver->notify(new FCMNotification('Calling..', 'Call from '.$user->name, [
+        $chat->customer->notify(new FCMNotification('Calling..', 'Call from '.$user->name, [
             'token'=>$token,
             'channel'=>$channel_name,
             'caller'=>$user->name,
