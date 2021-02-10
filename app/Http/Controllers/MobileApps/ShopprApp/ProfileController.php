866f4c35143fba900974cfb9f99bef43cb14aab5
alam <badre.alam1993@gmail.com>
2021-02-10 16:53:33 +0530

    shopper profile

--- app/Http/Controllers/MobileApps/ShopprApp/ProfileController.php
+++ app/Http/Controllers/MobileApps/ShopprApp/ProfileController.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace App\Http\Controllers\MobileApps\ShopprApp;
+
+use App\Http\Controllers\Controller;
+use App\Models\State;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function state(Request $request){
+
+        $request->validate([
+            'image'=>'required|image'
+        ]);
+        $user=$request->user;
+
+        $states=State::with('cities')->orderBy('id','desc')->get();
+
+        return [
+            'status'=>'success',
+            'message'=>'location updated',
+            'data'=>compact('states')
+        ];
+
+    }
+}
