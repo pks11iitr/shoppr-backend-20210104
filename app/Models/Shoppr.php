d84850632dd8c5340c62a89498f85be451dec56e
pankaj <pankaj@vm-jenkins-new.com>
2021-03-09 13:00:39 +0530

    logout apis

--- app/Models/Shoppr.php
+++ app/Models/Shoppr.php
@@ -19,7 +19,7 @@ class Shoppr extends Authenticatable implements JWTSubject
 
     protected $table='shoppers';
 
-    protected $fillable = ['mobile', 'name', 'status', 'lat','lang', 'isactive','location', 'notification_token','image','address','password','pan_card'.'sendbird_token','front_aadhaar_card','front_dl_no','account_no','ifsc_code','account_holder','bank_name','form_step','state','city','back_aadhaar_card','back_dl_no','pay_per_km','pay_commission','pay_delivery'];
+    protected $fillable = ['mobile', 'name', 'status', 'lat','lang', 'isactive','location', 'notification_token','image','address','password','pan_card'.'sendbird_token','front_aadhaar_card','front_dl_no','account_no','ifsc_code','account_holder','bank_name','form_step','state','city','back_aadhaar_card','back_dl_no','pay_per_km','pay_commission','pay_delivery','bike_front','bike_back', 'permanent_address', 'permanent_pin','permanent_city', 'seconday_mobile','emergency_mobile'];
 
     protected $hidden = ['deleted_at','updated_at','created_at'];
 
