d6f506bd2116240ebbdae4156dfa314a51d0df47
pankaj <pankaj@vm-jenkins-new.com>
2021-03-22 10:40:36 +0530

    chat details export, pending

--- app/Http/Controllers/MobileApps/Api/HomeController.php
+++ app/Http/Controllers/MobileApps/Api/HomeController.php
@@ -5,21 +5,39 @@ namespace App\Http\Controllers\MobileApps\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Notification;
 use App\Models\Shoppr;
+use App\Models\WorkLocations;
 use Illuminate\Http\Request;
 use DB;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         $user=auth()->guard('customerapi')->user();
-       $shopper= Shoppr::groupBy('location')->select(DB::raw('count(*) as shoppr_count, location'))->where('isactive',1)->get();
+        $location=WorkLocations::extractlocationfromjson($request->location);
+        if(!$location)
+            return [
+                'status'=>'failed',
+                'message'=>'Location is not servicable'
+            ];
+
+//       $shopper= Shoppr::groupBy('location')->select(DB::raw('count(*) as shoppr_count, location'))->where('isactive',1)->get();
+
+        $shopprs=Shoppr::active()
+            ->whereHas('locations', function($query)use($location) {
+                $query->where('name', $location->name);
+            })->count();
+
+        $shopper=[
+            'shopper_count'=>$shopprs??0,
+            'location'=>''
+        ];
 
         $notifications=Notification::where('user_type', 'CUSTOMER')
             ->where('user_id', $user->id)
             ->where('seen_at', null)
             ->count();
 
-       if($shopper->count()>0){
+       if($shopper['shopper_count']){
            return [
                'status'=>'success',
                'message'=>'success',
