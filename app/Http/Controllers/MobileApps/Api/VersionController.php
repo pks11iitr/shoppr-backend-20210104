294a49f56c32395ae47df29b5e6ccd287a47f2dd
pankaj <pankaj@vm-jenkins-new.com>
2021-03-24 11:10:22 +0530

    chat details export, pending

--- app/Http/Controllers/MobileApps/Api/VersionController.php
+++ app/Http/Controllers/MobileApps/Api/VersionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\MobileApps\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\AppVersion;
 use Illuminate\Http\Request;
 
 class VersionController extends Controller
