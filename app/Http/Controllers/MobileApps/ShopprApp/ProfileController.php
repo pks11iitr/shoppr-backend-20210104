a247e1aa420ad6ae3ef9dd9f5330111f826ea163
pankaj <pankaj@vm-jenkins-new.com>
2021-04-03 19:06:01 +0530

    title added in all notifications

--- app/Http/Controllers/MobileApps/ShopprApp/ProfileController.php
+++ app/Http/Controllers/MobileApps/ShopprApp/ProfileController.php
@@ -147,7 +147,7 @@ class ProfileController extends Controller
         }
 
         $orders=0;
-        if($work_locations){
+        if($user->isactive && $work_locations){
             $orders=Chat::where('shoppr_id', 0)
                 ->whereIn('location_id', $work_locations)
                 ->whereDoesntHave('rejectedby', function ($rejectedby) use ($user) {
