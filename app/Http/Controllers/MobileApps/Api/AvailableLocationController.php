1d1c903ea789000017095b5803f43c7d4929127e
pankaj <pankaj@vm-jenkins-new.com>
2021-04-24 22:08:59 +0530

    changes in location check

--- app/Http/Controllers/MobileApps/Api/AvailableLocationController.php
+++ app/Http/Controllers/MobileApps/Api/AvailableLocationController.php
@@ -47,11 +47,11 @@ class AvailableLocationController extends Controller
             $json = json_decode($location, true);
             if (count($json) >= 4) {
                 $json = array_reverse($json);
-                $locality1 = $json[2]['value'] ?? '';
-                $locality2 = $json[3]['value'] ?? '';
-                $locality3 = $json[4]['value'] ?? '';
-                $locality4 = $json[5]['value'] ?? '';
-                $locality5 = $json[6]['value'] ?? '';
+                $locality1 = $json[3]['long_name'] ?? '';
+                $locality2 = $json[4]['long_name'] ?? '';
+                $locality3 = $json[5]['long_name'] ?? '';
+                $locality4 = $json[6]['long_name'] ?? '';
+                $locality5 = $json[7]['long_name'] ?? '';
 
                 $location = WorkLocations::active()
                     ->where(function ($query) use ($locality1, $locality2, $locality3, $locality4, $locality5)
