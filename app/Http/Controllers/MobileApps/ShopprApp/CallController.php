ad396efbd3e20ba9c944f7a221e7308b2e304488
pankaj <pankaj@vm-jenkins-new.com>
2021-06-09 12:24:15 +0530

    fixes to send multiple tokens

--- app/Http/Controllers/MobileApps/ShopprApp/CallController.php
+++ app/Http/Controllers/MobileApps/ShopprApp/CallController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\MobileApps\ShopprApp;
 use App\Http\Controllers\Controller;
 use App\Models\Chat;
 use App\Models\Customer;
+use App\Models\Order;
 use App\Models\Shoppr;
 use App\Services\Agora\RtcTokenBuilder;
 use App\Services\Agora\RtmTokenBuilder;
@@ -59,7 +60,25 @@ class CallController extends Controller
         $user=$request->user;
 
         $chat=Chat::with('shoppr')
+            ->where('shoppr_id', $user->id)
             ->findOrFail($chat_id);
+
+        if($chat->is_terminated){
+            return [
+                'status'=>'failed',
+                'message'=>'You cannot make a call as this chat has been terminated'
+            ];
+        }
+
+        $order=Order::where('chat_id', $chat->id)
+            ->orderBy('id', 'desc')
+            ->first();
+
+        if($order->status=='Delivered')
+            return [
+                'status'=>'failed',
+                'message'=>'You cannot make a call as this order has been delivered'
+            ];
 
         $user_id='Customer-'.$chat->customer_id;
 
